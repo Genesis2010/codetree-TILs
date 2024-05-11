143bf9663a5864d1bdd3d6e8b2af7ff7dd5bc700
JunHyuk Park <45564605+Genesis2010@users.noreply.github.com>
2024-05-11 14:43:46 +0900

    Title: 점수대 파악하기, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240511/점수대 파악하기/find-out-the-score-range.swift
+++ 240511/점수대 파악하기/find-out-the-score-range.swift
@@ -3,10 +3,15 @@ let input = readLine()!.split(separator: " ").map{Int($0)!}
 var result = Array(repeating: 0, count: 10) 
 
 for i in input {
-    if i == 0 {
+    if i == 0  {
         break
     }
-    result[(i/10)-1] += 1
+
+    if i < 10 {
+        continue
+    } else {
+        result[(i/10)-1] += 1
+    }
 }
 
 for (index, value) in result.enumerated().reversed() {
